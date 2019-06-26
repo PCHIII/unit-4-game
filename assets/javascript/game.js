627ca8fdcf3491f063764a3695428ae597a80b6a
PCHIII <treyhelmer@gmail.com>
2019-06-26 13:21:15 -0400

    html layout, js-psuedo

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+// game has 4 crystals
+// each crystal gets a random number between 1 and 12
+// computer picks a random number between 90-120.
+// when you click on a crystal, the cystals number is added until it reaches total score
+// or goes above total score.
+// if not equall game retarts and add to loss
+// if equal game restarts and add to win
+// with every restart new numbers are generated.
+
+
