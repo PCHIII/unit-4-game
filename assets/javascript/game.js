7b09bea7ac2e6529b001e0043554962c08645a83
PCHIII <treyhelmer@gmail.com>
2019-07-01 21:21:19 -0400

    clean up beautify

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,6 @@
 // with every restart new numbers are generated.
 
 $(document).ready(function () {
-
     // hint button
     $("button").one('click', function () {
         $("#container").append("Click on each Gemstone first to determine their number value, after that its just simple addition!!");
@@ -21,22 +20,21 @@ $(document).ready(function () {
     var losses = 0;
 
     // create 4 variables for 4 crystals that equal a random number between 1-12
-    // add + 1 so that 0 is skipped over.
-
+    // add + 1 
     var crystal1 = Math.floor(Math.random() * 12) + 1;
     var crystal2 = Math.floor(Math.random() * 12) + 1;
     var crystal3 = Math.floor(Math.random() * 12) + 1;
     var crystal4 = Math.floor(Math.random() * 12) + 1;
 
-    // Generate computerNumber between 90-120; ad to computer number.
+    // Generate computerNumber between 90-120; ad to computer number ID.
     function getNumber() {
         computerNumber = Math.floor(Math.random() * 31) + 90;
         $("#computerNumber").html(computerNumber);
         
     };
-
     // Restart Game, reset number values
-    // update score and regenerate random numbers for crystals 1-12
+    // update score and re-generate random numbers for crystals 1-12 on restart
+    // clear alert
     function restartGame() {
         crystal1 = Math.floor(Math.random() * 12) + 1;
         crystal2 = Math.floor(Math.random() * 12) + 1;
@@ -48,38 +46,35 @@ $(document).ready(function () {
         $("#alert").html("")
     };
 
-    // Update the playersNumber index.html with userScore value
-    function updateScore() {
-        $("#userScore").html(userScore);
-
-    };
-
     // win = add win, update score, alert, start new game
+    // set a timeout delay to pause while alert is display
     function youWin() {
         updateScore();
         wins++;
         $("#wins").html("Wins: " + wins);
         $("#alert").html("YOU WIN!!")
-        setTimeout(restartGame, 3000);
-        // $("#alert").hide(3000);
-        // alert("You win!");
-        // restartGame();
+        setTimeout(restartGame, 2000);
+       
     };
 
     // lose = add loss, update score, alert, restart game
+    // set a timeout delay to pause while alert is display
     function youLose() {
         updateScore();
         losses++;
         $("#losses").html("Losses: " + losses);
         $("#alert").html("SORRY TRY AGAIN")
-        setTimeout(restartGame, 3000);
-        // $("#alert").hide(3000);
-        // alert("Sorry, Try Again");
-        // restartGame();
+        setTimeout(restartGame, 2000);
+        
     };
     
+    // Update the players Number to user score ID on index.html
+    function updateScore() {
+        $("#userScore").html(userScore);
+    };
 
     // Click function - Generate crystal1 value
+    // add crystals value to userscore ID
     $("#crystal1").click(function () {
         userScore += crystal1;
         updateScore();
@@ -134,7 +129,5 @@ $(document).ready(function () {
              youLose();
          }
     });
-
     restartGame();
-
 });
